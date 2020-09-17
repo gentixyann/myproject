bb954fcb0cbd952db437781be577234cf6c8b006
gentixyann <a551.return@gmail.com>
2020-09-17 15:41:54 +0900

    task編集できた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\CreateTask;
 use App\Folder;
 use App\Task;
+use App\Http\Requests\EditTask;
 
 
 class TaskController extends Controller
@@ -66,6 +67,23 @@ class TaskController extends Controller
 
 	    return view('tasks/edit', [
 	        'task' => $task,
+	    ]);
+	}
+
+	public function edit(int $id, int $task_id, EditTask $request)
+	{
+	    // 1
+	    $task = Task::find($task_id);
+
+	    // 2
+	    $task->title = $request->title;
+	    $task->status = $request->status;
+	    $task->due_date = $request->due_date = date('Y-m-d H:i:s');
+	    $task->save();
+
+	    // 3
+	    return redirect()->route('tasks.index', [
+	        'id' => $task->folder_id,
 	    ]);
 	}
 
